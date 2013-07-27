be281bcbe4aab25a266afa96d1b4619d69981639
Neil Hooey <nhooey@gmail.com>
2013-07-27 19:59:10 -0400

    Third-party shit.

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function() {
+
+Person = Backbone.Model.extend({
+  defaults: {
+    name: 'default'
+  },
+  initialize: function(){
+    console.log("Welcome to this world");
+    this.on("change:name", function(model) {
+      console.log("name changed to " + model.get("name"));
+    });
+  }
+});
+
+var person = new Person;
+person.set({name: "foo"})
+
+$("#tabs").tabs();
+
+});
