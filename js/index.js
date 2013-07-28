95f371e09c92eca8987505e32681879ddbb103b9
Neil Hooey <nhooey@gmail.com>
2013-07-27 23:29:41 -0400

    Removed search.

--- js/index.js
+++ js/index.js
@@ -67,29 +67,6 @@ App = Backbone.View.extend({
   },
 });
 
-SearchView = Backbone.View.extend({
-  initialize: function(){
-    this.render();
-  },
-  render: function(){
-    //Pass variables in using Underscore.js Template
-    var variables = { search_label: "My Search" };
-    // Compile the template using underscore
-    var template = _.template( $("#search_template").html(), variables );
-    // Load the compiled HTML into the Backbone "el"
-    this.$el.html( template );
-  },
-  events: {
-    "click input[type=button]": "doSearch"
-  },
-  doSearch: function( event ){
-    // Button clicked, you can access the element that was clicked with event.currentTarget
-    console.log( "Search for " + $("#search_input").val() );
-  }
-});
-
-var search_view = new SearchView({ el: $("#search_container") });
-
 new App();
 
 });
